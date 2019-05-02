f6ccca5d0bd84747023c4768f8203449befbed11
Eve Kovacs <kovacs@anl.gov>
2019-05-02 16:10:36 -0500

    Changes to accomodate different input file formats (fitres, txt)

--- ML_globals.py
+++ ML_globals.py
@@ -1,6 +1,6 @@
 # GLOBAL VARIABLES 
 
-name = 'ML_DES_typing'
+name = 'SNIRF (SN Identification with Random Forest)'
 authors = ['Eve Kovacs', 'Steve Kuhlmann', 'Ravi Gupta']
 
 # true types, classified types
@@ -49,6 +49,9 @@ Spec = 'Spec'
 Spec_nofp = 'Spec_nofp'
 Phot = 'Phot'
 User = 'User'
+
+datafile_keys = ['Training', 'Validation', 'Test', 'Spec', 'Spec_nofp', 'Phot']
+simulated_keys = ['Training', 'Validation', 'Test']
 
 # Missing data in Table  
 nodata = -999
@@ -110,6 +113,77 @@ Bazin = 'Bazin'
 #constants
 zlo = 0.0
 zhi = {Training: 1.4, Validation: 1.4, Test:1.4, Data: 1.1}
+
+#translation dict for different data formats
+
+allowed_formats = {'text':['txt'], 'fitres':['FITRES', 'csv']}
+
+default_format = 'text'
+
+allowed_features = {'text': ['c', 'x0', 'x1', 't0', 'z', 'chi2', 'fit_pr', 'gpeak', 'rpeak', 'ipeak', 'zpeak', 'ra',
+                             'dec', 'grpeak_m', 'ripeak_m', 'izpeak_m', 'grpeak_s', 'ripeak_s', 'izpeak_s',
+                             'Deltagr', 'Deltari', 'Deltaiz', 'snr1', 'snr2', 'snr3', 't0_err', 'x0_err', 'x1_err',
+                             'c_err', 'Bazin_g_t_rise', 'Bazin_g_t_fall', 'Bazin_r_t_rise', 'Bazin_r_t_fall',
+                             'Bazin_i_t_rise', 'Bazin_i_t_fall', 'Bazin_z_t_rise', 'Bazin_z_t_fall', 'grBazin',
+                             'riBazin', 'izBazin'],
+                    'fitres':['CID', 'IDSURVEY', 'TYPE', 'FIELD', 'CUTFLAG_SNANA', 'zHEL',
+                              'zHELERR', 'zCMB', 'zCMBERR', 'zHD', 'zHDERR', 'VPEC', 'VPECERR', 'HOST_LOGMASS',
+                              'HOST_LOGMASS_ERR', 'HOST_sSFR', 'HOST_sSFR_ERR', 'SNRMAX1', 'SNRMAX2', 'SNRMAX3', 'PKMJD',
+                              'PKMJDERR', 'x1', 'x1ERR', 'c', 'cERR', 'mB', 'mBERR', 'x0', 'x0ERR', 'COV_x1_c', 'COV_x1_x0',
+                              'COV_c_x0', 'NDOF', 'FITCHI2', 'FITPROB', 'SIM_TYPE_INDEX', 'SIM_TEMPLATE_INDEX', 'SIM_LIBID',
+                              'SIM_NGEN_LIBID', 'SIM_ZCMB', 'SIM_VPEC', 'SIM_DLMAG', 'SIM_PKMJD', 'SIM_SHAPE', 'SIM_AV',
+                              'SIMNULL1', 'SIM_RV', 'SIM_x0', 'SIM_mB'],
+                    }
+
+alternate_feature_names = {'text': {},
+                           'fitres':{'PKMJDERR':'PKMJDerr',
+                                    }
+                          }
+
+desired_class_values = {Ia:0, Ib:1, Ic:1, Ibc:1, II:2, CC:1}
+
+data_defaults = {'text':{'alltypes_colname':'type3',
+                         'target_class':[0],
+                         'type_labels':[Ia, II, Ibc, Ib, Ic],
+                         'type_values':[0, 2, 1, 1, 1],
+                         'type_colnames': {'2':'type', '3':'type3', '-2':['type2x2','type']},
+                         'alltypes_colname_default':'type3',
+                         'alltypes_available':True,
+                        },
+                 'fitres':{'alltypes_colname':'TYPE',
+                           'target_class':[1, 101],
+                           'type_labels':[Ia],
+                           'type_values':[[1, 101]],
+                           'type_colnames': {'2':'type', '3':'type3', '-2':['type2x2','type']}, #add entries for creation on the fly
+                           'alltypes_colname_default':'TYPE',  #only binary typing available
+                           'alltypes_available':False,
+                          }
+                }
+
+feature_names = {'z':{'text':'z', 'fitres':'zCMB'},
+                 'snr1':{'text':'snr1', 'fitres':'SNRMAX1'},
+                 'sim_type':{'text':'sim_type', 'fitres':'SIM_TYPE_INDEX'},
+                 'sim_template':{'text':'sim_nonIa', 'fitres':'SIM_TEMPLATE_INDEX'},
+                 'snid':{'text':'snid','fitres':'CID'},                 
+                } 
+
+translate = {'txt_2_fitres':{'snid':'CID', 'z': 'zCMB', 't0':'PKMJD', 't0_err':'PKMJDERR',
+                             'x0_err':'x0ERR', 'x1_err':'x1ERR', 'c_err':'cERR', 'chi2':'FITCHI2', 'dof':'NDOF', 
+                             'fit_pr':'FITPROB', 'snr1':'SNRMAX1', 'snr2':'SNRMAX2', 'snr3':'SNRMAX3', 'field':'FIELD',
+                             'sim_type':'SIM_TYPE_INDEX', 'sim_idx':'SIM_TEMPLATE_INDEX',
+                             }
+            }
+
+allowed_templates = {'2P':['20'], '2N':['21'], '2L':['22'], '1b':['32'], '1c':['33'],
+                     'IIN':['206', '209'],
+                     'IIL':['002'], 
+                     'IIP':['201', '204', '208', '210', '213', '214', '215', '216', '219', '220', '221', '222',
+                            '223', '224', '225', '226', '227', '228', '229', '230', '231', '232', '233', '235'],
+                     'Ib':['103', '104', '105', '202', '203', '212', '234'],
+                     'Ic':['021', '022', '101', '102', '205', '207', '211', '217', '218'],
+                     'TBD':['400','401', '502', '503'],
+                    }
+
 
 ################ FUNCTIONS ##############
 
